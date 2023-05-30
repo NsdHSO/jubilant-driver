aab1f907bc02214450917c09fffbbe42802ccbd7
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-05-30 14:46:14 +0300

    chore:🌻 added powerfull of async pipe

--- apps/driver/src/app/remote-entry/landing-driver/components/landing/landing.component.ts
+++ apps/driver/src/app/remote-entry/landing-driver/components/landing/landing.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataSourceMaterialTable } from "ngx-liburg";
 import { LandingService } from "../../utils/landing.service";
-import { Subject } from "rxjs";
 import { ActivatedRoute } from "@angular/router";
 
 export interface Driver {
@@ -18,35 +17,29 @@ export interface Driver {
 @Component({
   selector: 'driver-landing',
   templateUrl: './landing.component.html',
-  styleUrls: [ './landing.component.scss' ],
+  styleUrls: [ './landing.component.scss' ]
 })
-export class LandingComponent implements OnInit {
-  dataSource: DataSourceMaterialTable<any>[] | undefined
- 
-  private _destroyed = new Subject();
+export class LandingComponent {
 
-  constructor(
+  dataSource = this._activatedRoute.snapshot.data[ 'drivers' ].map((driver: any) => {
+    const model = {
+      ...driver
+    }
+    return {
+      actions: [ {
+        iconClass: "fa_solid:gauge",
+        classCss: "edit",
+        method: (row: Driver) => console.log(row)
+      } ],
+      editable: true,
+      model: {
+        ...model
+      }
+    } as DataSourceMaterialTable<any>
+  })
+
+  constructor (
     private readonly _landingService: LandingService,
     private readonly _activatedRoute: ActivatedRoute){
-  }
-
-  ngOnInit(): void{
-
-    this.dataSource = this._activatedRoute.snapshot.data[ 'drivers' ].map((driver: any) => {
-      const model = {
-        ...driver
-      }
-      return {
-        actions: [ {
-          iconClass: "fa_solid:gauge",
-          classCss: "edit",
-          method: (row: Driver) => console.log(row)
-        } ],
-        editable: true,
-        model: {
-          ...model
-        }
-      } as DataSourceMaterialTable<any>
-    })
   }
 }
